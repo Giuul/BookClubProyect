b97d3be974bbc95205775dd1f704039a300e643d
Giuli :) <113950045+Giuul@users.noreply.github.com>
2025-10-30 13:43:34 -0300

    Agrego Dtos

---
+++ Application/Models/ReadingListDTO.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class ReadingListDTO
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string? Descripcion { get; set; }
+        public bool EsCompartida { get; set; }
+        public int CreadorId { get; set; }
+        public string CreadorNombre { get; set; } = string.Empty;
+
+        public static ReadingListDTO Create(ReadingList list)
+            => new ReadingListDTO
+            {
+                Id = list.Id,
+                Titulo = list.Titulo,
+                Descripcion = list.Descripcion,
+                EsCompartida = list.EsCompartida,
+                CreadorId = list.CreadorId,
+                CreadorNombre = list.Creador.Nombre
+            };
+
+        public static List<ReadingListDTO> CreateList(IEnumerable<ReadingList> lists)
+            => lists.Select(Create).ToList();
+    }
+}
