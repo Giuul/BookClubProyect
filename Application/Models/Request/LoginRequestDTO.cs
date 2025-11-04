b7678474ba84c5138d52b2bffd9313fa9c6fdc0c
Giuli :) <113950045+Giuul@users.noreply.github.com>
2025-11-03 22:45:06 -0300

    Update LoginRequestDTO, y AuthService

--- Application/Models/Request/LoginRequestDTO.cs
+++ Application/Models/Request/LoginRequestDTO.cs
@@ -1,9 +1,19 @@
-﻿namespace Application.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models
 {
     public class LoginRequestDTO
     {
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [MinLength(2, ErrorMessage = "El nombre debe tener al menos 2 caracteres.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El formato del correo electrónico no es válido.")]
         public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
+        [MinLength(6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres.")]
         public string Password { get; set; } = string.Empty;
     }
 }
-
