3d4bb54ae6e1d986064a879138043ce39ffdf8b2
Giuli :) <113950045+Giuul@users.noreply.github.com>
2025-11-07 21:03:23 -0300

    Preparing for gitHub action deployment

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -110,6 +110,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+
 app.UseHttpsRedirection();
 
 app.UseAuthentication(); 
