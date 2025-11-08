4be0d875496278bccbea10639e95136911240030
valenggar <valentinagarcia11230@gmail.com>
2025-11-07 21:43:48 -0300

    subo appsettings

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -74,10 +74,11 @@ builder.Services.AddSwaggerGen(options =>
 
 
 // Configuración de la Base de Datos (MySQL)
-string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+string connectionString = Environment.GetEnvironmentVariable("DefaultConnection")
+    ?? throw new Exception("La variable de entorno DefaultConnection no está configurada.");
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 // Configuración de JWT
 var jwtKey = builder.Configuration["Jwt:Key"] ??
