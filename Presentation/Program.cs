f2614541dbdf6515214f0049bb80fe5ce9061c89
Giuli :) <113950045+Giuul@users.noreply.github.com>
2025-11-07 22:16:36 -0300

    Modifico program.cs para que swagger funcione tambien en produccion

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -104,7 +104,7 @@ builder.Services.AddAuthentication("Bearer")
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment()|| app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
